3cee93c866dcfca190894dc905f94230b2b6575e
Kaushal Rathi <kaushalrathi24@gmail.com>
2022-10-01 05:03:37 +0530

    fix: added text to message schema

--- backend/models/message.js
+++ backend/models/message.js
@@ -1,11 +1,16 @@
 import { Schema, model } from "mongoose";
 
-const message = new Schema({
+const messageSchema = new Schema({
   _id: {
     type: String,
     default: () => randomUUID(),
   },
   sentBy: { type: String, required: true },
+  text: { type: String, required: true },
   chatRoom: { type: String, required: true },
   createdAt: { type: String, default: Date.now },
 });
+
+const Message = model("Message", messageSchema);
+
+export default Message;
